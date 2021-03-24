59f2f20221070cad62b1998db3974d9cad01aea2
DmDespert <ordimac@jim-dede.(none)>
2021-03-24 09:39:15 +0100

    feat: exo-1 terminé

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,1 +1,11 @@
 console.log('exercice 1');
+
+// jQuery est une simplification d'écriture du JS, cependant ce language est destiné à ne plus être utilisé.
+
+$(document).ready(function(){ //Javascript "page chargée", on lance la fonction :
+    console.log('Je peux maintenant écrire du code jQuery');
+});
+
+// La première ligne permet de s'assurer que la page est bien chargée avec de pouvoir lancer les scripts
+// comme la modification du html/css, le chargement de script supplémentaires, etc...
+// En effet, la balise $ charge le jQuery.
